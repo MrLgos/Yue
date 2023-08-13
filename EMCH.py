88d41315831e3607de400229f56b2821cb19d28b
MrLgos <LinJw970@gmail.com>
2023-08-13 16:40:59 +0800

    Update threading, Online ally check.

--- EMCH.py
+++ EMCH.py
@@ -1,14 +1,56 @@
 import requests
+import threading
+import time
+from datetime import datetime
+from time import strftime
+from time import gmtime
+from dateutil.parser import parse
+from concurrent.futures import ThreadPoolExecutor
 
-def Query_Unallied():
-    nation = input("请输入你想查询的国家名:")
-    cnt=0
+MAX_REQUESTS_PER_SECOND = 10
+REQUEST_INTERVAL = 1 / MAX_REQUESTS_PER_SECOND
 
+def send_request(town):
+    url = f"https://api.earthmc.net/v1/aurora/towns/{town}"
+    print(town)
+    resNum = 0
+    try:
+        townsLookup = requests.get(url).json()
+        mayor = townsLookup['strings']['mayor']
+        x = townsLookup['spawn']['x']
+        z = townsLookup['spawn']['z']
+        town_size = townsLookup['stats']['numTownBlocks']
+        is_ruined = townsLookup['status']['isRuined']
+        resNum = townsLookup['stats']['numResidents']
+    except Exception as e:
+        print(e)
+    if resNum == 1:
+        try:
+            mayorLookup = requests.get(f"https://api.earthmc.net/v1/aurora/residents/{mayor}").json()
+        except Exception as e:
+            print(e)
+        lastOnline_TimeStamp = mayorLookup['timestamps']['lastOnline'] / 1000
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        last = datetime.fromtimestamp(lastOnline_TimeStamp).strftime('%Y-%m-%d %H:%M:%S')
+        date_now = parse(now)
+        date_last = parse(last)
+        result = (date_now - date_last).total_seconds()
+        m, s = divmod(result, 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
+        if d == 42 and is_ruined == False:
+            with open('towns.txt', 'a+', encoding="utf-8") as f:
+                f.write(
+                    "Town: %s \nChunks: %d\nMayor: %s\nOffline since %d days %d hours %d minutes %d seconds.\ndynmap URL：https://earthmc.net/map/aurora/?worldname=earth&mapname=flat&zoom=5&x=%d&z=%d\n\n" % (
+                    town, town_size, mayor, d, h, m, s, x, z))
+            f.close()
+
+def Query_Unallied(nation):
     allNationsLookup = requests.get("https://api.earthmc.net/v1/aurora/nations/").json()
     try:
         nationsLookup = requests.get(f"https://api.earthmc.net/v1/aurora/nations/{nation}").json()
     except:
-        print("错误的输入请重试.")
+        print("Invalid Input.")
         return
 
     allyList = nationsLookup["allies"]
@@ -17,11 +59,7 @@ def Query_Unallied():
 
     unalliedList = list(set(allNations).difference(set(allyList)))
     unalliedList.sort()
-    for name in unalliedList:
-        print(name)
-        cnt += 1
-    print("[%s 有上述 %d 个国家尚未结盟.]"%(nation,cnt))
-    return
+    return unalliedList
 
 def Query_Falling_Nations():
     import time
@@ -51,73 +89,67 @@ def Query_Falling_Nations():
             h, m = divmod(m, 60)
             d, h = divmod(h, 24)
             if d >= 35:
-                print ("国家名: %s\n国王: %s\n已经离线 %d 天 %d 小时 %d 分钟 %d 秒." % (nation, king, d, h, m, s))
+                print ("Nation: %s\nKing: %s\nOffline since %d days %d hours %d minutes %d seconds." % (nation, king, d, h, m, s))
                 cnt += 1
     # AuroraLookup = requests.get("https://api.earthmc.net/v1/aurora/").json()
     # time = AuroraLookup["world"]["fullTime"]
-    print("将有 %d 个国家即将倒闭. 请在游戏中使用/seen指令进一步明确!" % cnt)
+    print(" %d nations about to fall. Use /seen in game to check more details!" % cnt)
 
 def Query_Falling_Towns():
-    import time
-    from datetime import datetime
-    from time import strftime
-    from time import gmtime
-    from dateutil.parser import parse
-    cnt = 0 # 计算将要倒闭的城镇总数
-    sum = 0 # 计算已经遍历了多少个城镇
     allTownsLookup = requests.get("https://api.earthmc.net/v1/aurora/towns/").json()
-    numTowns = allTownsLookup["numTowns"]
+    # numTowns = allTownsLookup["numTowns"]
     allTowns = allTownsLookup["allTowns"]
-    for town in allTowns:
-        try:
-            townsLookup = requests.get(f"https://api.earthmc.net/v1/aurora/towns/{town}").json()
-            mayor = townsLookup['strings']['mayor']
-            x = townsLookup['spawn']['x']
-            y = townsLookup['spawn']['y']
-            z = townsLookup['spawn']['z']
-            town_size = townsLookup['stats']['numTownBlocks']
-            is_ruined = townsLookup['status']['isRuined']
-            resNum = townsLookup['stats']['numResidents']
-        except:
-            continue
-        sum += 1
-        print("%s %.2lf%%" % (town,sum / numTowns * 100))
-        if resNum == 1:
+    total_requests = len(allTowns)
+    with ThreadPoolExecutor(max_workers=total_requests) as executor:
+        for town in allTowns:
             try:
-                mayorLookup = requests.get(f"https://api.earthmc.net/v1/aurora/residents/{mayor}").json()
+                executor.submit(send_request, town)
+                time.sleep(REQUEST_INTERVAL)
             except:
                 continue
-            lastOnline_TimeStamp = mayorLookup['timestamps']['lastOnline']/1000
-            # lastOnline_TimeTuple = time.localtime(lastOnline_TimeStamp)
-            now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            last = datetime.fromtimestamp(lastOnline_TimeStamp).strftime('%Y-%m-%d %H:%M:%S')
-            date_now = parse(now)
-            date_last = parse(last)
-            result = (date_now - date_last).total_seconds()
-            m, s = divmod(result, 60)
-            h, m = divmod(m, 60)
-            d, h = divmod(h, 24)
-            if d >= 41 and is_ruined == False:
-                with open('结果.txt','a+',encoding="utf-8") as f: 
-                    f.write("城镇名字: %s 城镇区块数: %d\n市长: %s\n已经离线 %d 天 %d 小时 %d 分钟 %d 秒.\ndynmap链接：https://earthmc.net/map/aurora/?worldname=earth&mapname=flat&zoom=5&x=%d&z=%d\n是否是废弃状态: %s\n\n" % (town, town_size, mayor, d, h, m, s, x, z, is_ruined))
-                f.close()
-                cnt += 1
-    # AuroraLookup = requests.get("https://api.earthmc.net/v1/aurora/").json()
-    # time = AuroraLookup["world"]["fullTime"]
-    print("将有 %d 个城镇即将倒闭. 请在游戏中使用/seen指令进一步明确!" % cnt)
+    print("Check done. Use /seen in game to check more details!")
 
+def Query_leaders():
+    myNation = input("Your Nation:")
+    onlineLookup = requests.get("https://emctoolkit.vercel.app/api/aurora/onlineplayers").json()
+    nationLookup = requests.get("https://api.earthmc.net/v2/aurora/nations").json()
+    unalliedNation = Query_Unallied(myNation)
+    leader_nation = {}
+    for n in nationLookup:
+        nation = n["name"]
+        leader_nation[n["king"]] = nation
+        if "ranks" in n:
+            if "Chancellor" in n["ranks"]:
+                for chancellor in n["ranks"]["Chancellor"]:
+                    leader_nation[chancellor] = nation
+            if "Diplomat" in n["ranks"]:
+                for diplomat in n["ranks"]["Diplomat"]:
+                    if diplomat not in leader_nation:
+                        leader_nation[diplomat] = nation
+    for player in onlineLookup:
+        if player["name"] in leader_nation:
+            if leader_nation[player["name"]] in unalliedNation:
+                print("%s %s" % (player["name"], leader_nation[player["name"]]))
 
 if __name__ == '__main__':
-    print("1代表查询未结盟的国家\n2代表查询将要倒闭的国家或城镇\n3代表退出该程序")
+    print("1.Query unallied nations\n2.Query falling nations/towns\n3.Query online kings/chancellors/diplomats\n4.quit")
     while True:
-        index = input("请输入上述的一个数字:")
+        index = input("Input:")
         if index == "1":
-            Query_Unallied()
+            myNation = input("Your Nation:")
+            unallied = Query_Unallied(myNation)
+            cnt = 0
+            for name in unallied:
+                print(name)
+                cnt += 1
+            print("[%s have %d nations not allied.]" % (myNation, cnt))
         elif index == "2":
-            q = input("1代表查询将要倒闭的国家 2代表查询将要倒闭的城镇\n请输入上述的一个数字:")
+            q = input("1.Query nations 2.Query towns\nInput:")
             if q == "1":
                 Query_Falling_Nations()
             elif q == "2":
                 Query_Falling_Towns()
         elif index == "3":
+            Query_leaders()
+        elif index == "4":
             break
